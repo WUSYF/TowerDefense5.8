1b76dfba411a5d4879f90bfc29c3d875edc050ff
WUSYF <daniel.hofma@gmail.com>
2019-03-08 12:20:36 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet
+{
+
+    public static void main(String[] args) {
+        PApplet.main("Main.java");
+    }
+
+    public void settings()
+    {
+
+    }
+
+
+    public void setup()
+    {
+
+    }
+    public void draw()
+    {
+
+    }
+}
